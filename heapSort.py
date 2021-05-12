e4e72e09dc7479de8b630c9783534e7ec0eb49ec
Youssef Abdellatif <yabdellatifx@gmail.com>
2021-05-12 18:48:40 +0200

    bubble+heap

---
+++ heapSort.py
@@ -0,0 +1,27 @@
+def heapify(arr, num, i):
+    largest = i
+    right = 2 * i + 1
+    left = 2 * i + 2
+
+    if left < num and arr[largest] < arr[left]:
+        largest = left
+
+    if right < num and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        heapify(arr, num, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
---
+++ mergeSort.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+
+def merge(arr, left, right):
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+        k = k + 1
+    while i < len(left):
+        arr[k] = left[i]
+        k = k + 1
+        i = i + 1
+    while j < len(right):
+        arr[k] = right[j]
+        k = k + 1
+        j = j + 1
+    return arr
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        middle = len(arr)//2
+        left = arr[:middle]
+        right = arr[middle:]
+        merge_sort(left)
+        merge_sort(right)
+        arr = merge(arr, left, right)
+    return arr
