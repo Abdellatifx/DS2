95923b884b9b9137fdcf129e13f1bcca8281b40c
Mohamed Raslan <raslanmohamed.mr@gmail.com>
2021-05-14 17:57:29 +0200

    plotting done

--- SortingFunc.py
+++ SortingFunc.py
@@ -12,16 +12,16 @@ def arraygeneration(size):
 
 def printarray(arr, n, type):
     print("The", type, "sorted array with size ", n, "is ")
-    for i in range(0, n):
-        print("array[", i, "] = ", arr[i])
+    #for i in range(0, n):
+      #  print("array[", i, "] = ", arr[i])
 
 
 def sorter(method, type):
-    N = 6
+    N = 5
     ar = [0] * N
     j = 0
     n = []
-    for i in range(1, 6):
+    for i in range(1, 5):
         n.append(10 ** i)
         arr = arraygeneration(n[i-1])
         start_time = time.time()
@@ -29,6 +29,5 @@ def sorter(method, type):
         end_time = time.time()
         ar[i-1] = (end_time - start_time)
         printarray(arr, n[i-1], type)
-        print("Sorting time  for size", n[i-1], "is ", end_time - start_time)
-
+       # print("Sorting time  for size", n[i-1], "is ", end_time - start_time)
     return ar
